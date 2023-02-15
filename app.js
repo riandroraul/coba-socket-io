a715121bdecf739da863be9d3c6f270908473e27
riandroraul <riandroraul@gmail.com>
2023-02-15 22:34:17 +0700

    update

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const app = require("express")();
+const { log } = require("console");
 const ejsLayout = require("express-ejs-layouts");
 const port = 5000;
+
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.set("view engine", "ejs");
 app.set("view");
@@ -10,4 +16,11 @@ app.get("/", (req, res) => {
   res.render("index", { title: "Halaman Home", layout: "layouts/main-layout" });
 });
 
-app.listen(port, () => console.log(`listening at http://localhost:${port}`));
+io.on("connection", (socket) => {
+  socket.on("message", (data) => {
+    // console.log(data);
+    socket.broadcast.emit("message", data);
+  });
+});
+
+server.listen(port, () => console.log(`listening at http://localhost:${port}`));
